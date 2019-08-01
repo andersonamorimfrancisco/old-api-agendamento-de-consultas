e3e4ef65b44e997a0ea27e95c5b9f2925c2a88dc
Anderson Amorim <user.anderson@gmail.com>
2019-07-31 21:14:56 -0300

    Preparando estrutura para deploy

--- src/app.js
+++ src/app.js
@@ -5,8 +5,10 @@ const app = express();
 
 const routes = require("./routes");
 
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000);
+app.listen(PORT);
