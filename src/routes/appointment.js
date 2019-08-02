117cf76498d9a7fc8fee7e540c30f03326532ee3
Anderson Amorim <user.anderson@gmail.com>
2019-08-01 23:51:59 -0300

    Inserindo rota insertPatient

--- src/routes/appointment.js
+++ src/routes/appointment.js
@@ -4,8 +4,15 @@ const Appointment = require("../models/Appointment");
 const router = express.Router();
 
 router.get("/list", async (req, res) => {
-  const Appointments = await Appointment.find({});
+  const Appointments = await Appointment.find({}).populate("patient");
   res.json(Appointments);
+});
+router.post("/insertpatient", async (req, res) => {
+  await Appointment.findByIdAndUpdate(req.body.appointmentId, {
+    patient: req.body.patientId
+  });
+  const appointment = await Appointment.findById(req.body.appointmentId).populate("patient");
+  res.json(appointment);
 });
 
 module.exports = router;
