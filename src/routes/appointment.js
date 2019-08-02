6df015d6732551273994c989837dea50df5b13b1
Anderson Amorim <user.anderson@gmail.com>
2019-08-02 17:27:36 -0300

    Incluindo rota de remover paciente do compromisso

--- src/routes/appointment.js
+++ src/routes/appointment.js
@@ -7,6 +7,15 @@ router.get("/list", async (req, res) => {
   const Appointments = await Appointment.find({}).populate("patient");
   res.json(Appointments);
 });
+
+router.post("/removepatient", async (req, res) => {
+  await Appointment.findByIdAndUpdate(req.body._id, {
+    patient: null
+  });
+  const appointment = await Appointment.findById(req.body._id);
+  res.json(appointment);
+});
+
 router.post("/insertpatient", async (req, res) => {
   await Appointment.findByIdAndUpdate(req.body.appointmentId, {
     patient: req.body.patientId
